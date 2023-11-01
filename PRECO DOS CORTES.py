12d417d17f8c76ea1fd0ea756c22639f82eb4b8e
Prestocoast123 <149581091+Prestocoast123@users.noreply.github.com>
2023-11-01 08:19:41 -0300

    Update PRECO DOS CORTES.py

--- PRECO DOS CORTES.py
+++ PRECO DOS CORTES.py
@@ -5,3 +5,22 @@ class Cabelo:
      _preco: float
 
      def _init_(self, id, nome, preco):
+         self._nome = nome
+         self._preco = preco
+         self._id = id
+
+     @property
+     def nome(self):
+         return self._nome
+
+      @nome.setter
+      def nome(self, ):
+          self._nome = nome
+
+      @property
+      def preco(self):
+          return self._id
+
+      @preco.setter
+      def preco(self, preco):
+          self._preco = preco
